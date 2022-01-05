18fb693a0837db33ab587ba8d51354dc095ee93d
Ulli Hafner <ullrich.hafner@gmail.com>
2022-01-05 16:59:00 +0100

    Fix tests on Windows.

--- src/test/java/io/jenkins/plugins/prism/FilePermissionEnforcerTest.java
+++ src/test/java/io/jenkins/plugins/prism/FilePermissionEnforcerTest.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.prism;
 import java.io.File;
 
 import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.Issue;
 
 import hudson.FilePath;
 
@@ -16,36 +17,53 @@ import static org.assertj.core.api.Assumptions.*;
  * @author Ullrich Hafner
  */
 class FilePermissionEnforcerTest {
-    private static final FilePath WORKSPACE = new FilePath(new File("/workspace"));
+    private static final FilePath WORKSPACE_UNIX = new FilePath(new File("/workspace"));
+    private static final FilePath WORKSPACE_WINDOWS = new FilePath(new File("C:\\workspace"));
 
     @Test
     void shouldComparePathsOnUnix() {
-        FilePermissionEnforcer validator = new FilePermissionEnforcer();
-        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE, "/a")).isTrue();
-        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE, "/")).isTrue();
+        assumeThat(isWindows()).isFalse();
 
-        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE, "/a/b")).isFalse();
-        assertThat(validator.isInWorkspace("/b/a/b.c", WORKSPACE, "/a")).isFalse();
+        FilePermissionEnforcer validator = new FilePermissionEnforcer();
+        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE_UNIX, "/a")).isTrue();
+        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE_UNIX, "/")).isTrue();
+
+        assertThat(validator.isInWorkspace("/a/b.c", WORKSPACE_UNIX, "/a/b")).isFalse();
+        assertThat(validator.isInWorkspace("/b/a/b.c", WORKSPACE_UNIX, "/a")).isFalse();
     }
 
     @Test
-    void shouldAllowWorkspaceByDefault() {
+    void shouldAllowWorkspaceByDefaultOnUnix() {
+        assumeThat(isWindows()).isFalse();
+
         FilePermissionEnforcer validator = new FilePermissionEnforcer();
-        assertThat(validator.isInWorkspace("/workspace/b.c", WORKSPACE, "/a")).isTrue();
-        assertThat(validator.isInWorkspace("/b/workspace/b.c", WORKSPACE, "/a")).isFalse();
-        assertThat(validator.isInWorkspace("b.c", WORKSPACE, "/a")).isFalse();
+        assertThat(validator.isInWorkspace("/workspace/b.c", WORKSPACE_UNIX, "/a")).isTrue();
+        assertThat(validator.isInWorkspace("/b/workspace/b.c", WORKSPACE_UNIX, "/a")).isFalse();
+        assertThat(validator.isInWorkspace("b.c", WORKSPACE_UNIX, "/a")).isFalse();
     }
 
-    @Test @org.jvnet.hudson.test.Issue("JENKINS-63782")
+    @Test @Issue("JENKINS-63782")
     void shouldComparePathsCaseInsensitiveOnWindows() {
         assumeThat(isWindows()).isTrue();
 
         FilePermissionEnforcer validator = new FilePermissionEnforcer();
 
-        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE, "C:\\a")).isTrue();
-        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE, "C:\\")).isTrue();
-        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE, "C:\\A")).isTrue();
-        assertThat(validator.isInWorkspace("C:\\A\\b.c", WORKSPACE, "C:\\a")).isTrue();
-        assertThat(validator.isInWorkspace("c:\\a\\b.c", WORKSPACE, "C:\\a")).isTrue();
+        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE_WINDOWS, "C:\\a")).isTrue();
+        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE_WINDOWS, "C:\\")).isTrue();
+        assertThat(validator.isInWorkspace("C:\\a\\b.c", WORKSPACE_WINDOWS, "C:\\A")).isTrue();
+        assertThat(validator.isInWorkspace("C:\\A\\b.c", WORKSPACE_WINDOWS, "C:\\a")).isTrue();
+        assertThat(validator.isInWorkspace("c:\\a\\b.c", WORKSPACE_WINDOWS, "C:\\a")).isTrue();
+
+        assertThat(validator.isInWorkspace("c:\\a\\b.c", WORKSPACE_WINDOWS, "C:\\b")).isFalse();
+    }
+
+    @Test
+    void shouldAllowWorkspaceByDefaultOnWindows() {
+        assumeThat(isWindows()).isTrue();
+
+        FilePermissionEnforcer validator = new FilePermissionEnforcer();
+        assertThat(validator.isInWorkspace("C:\\workspace\b.c", WORKSPACE_WINDOWS, "C:\\a")).isTrue();
+        assertThat(validator.isInWorkspace("C:\\a\\workspace\\b.c", WORKSPACE_WINDOWS, "C:\\b")).isFalse();
+        assertThat(validator.isInWorkspace("b.c", WORKSPACE_WINDOWS, "C:\\a")).isFalse();
     }
 }
